f1c744a6dbfc4e004930006ac57513c6ccaee00a
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2012-09-08 11:36:40 +0200

    add startup script for development

--- apps/glacier_proxy/src/glacier_proxy.erl
+++ apps/glacier_proxy/src/glacier_proxy.erl
@@ -1,9 +1,21 @@
--module(glacier_proxy_app).
+-module(glacier_proxy).
 
 -behaviour(application).
 
+%% API
+-export([start/0, stop/0]).
+
 %% Application callbacks
 -export([start/2, stop/1]).
+
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start() -> application:start(?MODULE).
+stop()  -> application:stop(?MODULE).
+
 
 %% ===================================================================
 %% Application callbacks
