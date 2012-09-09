d3afabc8e1950b6ef18ff5a1780b738aa8b64147
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2012-09-09 22:37:44 +0200

    export gp_chksum:hexdigest/1

--- apps/glacier_proxy/src/gp_chksum.erl
+++ apps/glacier_proxy/src/gp_chksum.erl
@@ -5,6 +5,7 @@
 -export([sha256/1, sha256_digest/1, sha256_init/0, sha256_update/2, sha256_final/1]).
 -export([hmac256/2, hmac256_digest/2]).
 -export([tree_hash/1]).
+-export([hexdigest/1]).
 
 
 %% ===================================================================
@@ -18,11 +19,11 @@ sha_update(Context, Data) ->
     crypto:sha_update(Context, Data).
 
 sha_final(Context) ->
-    bin_to_hex(crypto:sha_final(Context)).
+    hexdigest(crypto:sha_final(Context)).
 
 
 sha256(Data) ->
-    bin_to_hex(sha256_digest(Data)).
+    hexdigest(sha256_digest(Data)).
 
 sha256_digest(Data) ->
     erlsha2:sha256(Data).
@@ -34,18 +35,18 @@ sha256_update(Context, Data) ->
     erlsha2:sha256_update(Context, Data).
 
 sha256_final(Context) ->
-    bin_to_hex(erlsha2:sha256_final(Context)).
+    hexdigest(erlsha2:sha256_final(Context)).
 
 
 hmac256(Key, Data) ->
-    bin_to_hex(hmac256_digest(Key, Data)).
+    hexdigest(hmac256_digest(Key, Data)).
 
 hmac256_digest(Key, Data) ->
     hmac:hmac256(Key, Data).
 
 
 tree_hash(L) ->
-    bin_to_hex(tree_hash_digest(L)).
+    hexdigest(tree_hash_digest(L)).
 
 tree_hash_digest([Root]) ->
     Root;
@@ -60,10 +61,10 @@ tree_hash_digest([], NextLevel) ->
     tree_hash_digest(lists:reverse(NextLevel)).
 
 
+hexdigest(BinDigest) ->
+    iolist_to_binary([[io_lib:format("~2.16.0b",[X]) || <<X:8>> <= BinDigest]]).
+
+
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
-
-
-bin_to_hex(BinDigest) ->
-    iolist_to_binary([[io_lib:format("~2.16.0b",[X]) || <<X:8>> <= BinDigest ]]).
